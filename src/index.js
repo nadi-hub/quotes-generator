9da078ae52ae46f050dc2a670a012d257f95e32e
nadi-hub <nadja_reisacher@hotmail.com>
2024-03-01 10:48:12 +0100

    added the AI API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-function generateQuote(event) {
-  event.preventDefault();
-
-  new Typewriter("#poem", {
-    strings: ["love is beautiful"],
+function displayQuote(response) {
+  console.log("quote generated");
+  new Typewriter("#quote", {
+    strings: response.data.answer,
     autoStart: true,
     cursor: null,
     delay: 1,
   });
+}
+
+function generateQuote(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#instruction-text");
+
+  let key = "fed5efb5810a3ce4atecb36bcacfoaa9";
+  let context = "You can create nice quotes";
+  let prompt = `Create a 4 lines quote about ${instructionsInput.value} in basic HTML. Separate each line with a <br />. After the quote sign "SheCodes AI" in italic and in blue color. Do not use quotation marks`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+
+  console.log("Generating quote");
+  console.log(`Prompt is ${prompt}`);
+  console.log(`Context is ${context}`);
+
+  axios.get(apiUrl).then(displayQuote);
 }
 
 let peomFormElement = document.querySelector("#quote-generator-form");
